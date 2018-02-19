7ef3e2c001825571aaa8a03e9d196a38d3c44b08
albertlangademy <albertlangademy@outlook.com>
2018-02-19 16:55:25 +0100

    commons

--- Views/BookLessons/Index.Designer.cs
+++ Views/BookLessons/Index.Designer.cs
@@ -19,7 +19,7 @@ namespace Resources.Views.BookLessons {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Index {
@@ -57,6 +57,15 @@ namespace Resources.Views.BookLessons {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Only students can purchase lessons. Follow the next link to activate your student profile.
+        /// </summary>
+        public static string ActiStudBefore {
+            get {
+                return ResourceManager.GetString("ActiStudBefore", resourceCulture);
             }
         }
         
