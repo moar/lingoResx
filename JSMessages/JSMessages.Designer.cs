becf480f362318f0aa834884cca6e425788fdbf9
aagirre <txiri2@msn.com>
2018-03-31 17:48:10 +0200

    i18n mensaje scheduler

--- JSMessages/JSMessages.Designer.cs
+++ JSMessages/JSMessages.Designer.cs
@@ -19,7 +19,7 @@ namespace Resources.JSMessages {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class JSMessages {
@@ -282,15 +282,6 @@ namespace Resources.JSMessages {
         public static string Confirm {
             get {
                 return ResourceManager.GetString("Confirm", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to .
-        /// </summary>
-        public static string ConfirmLessonPurchaseCancel {
-            get {
-                return ResourceManager.GetString("ConfirmLessonPurchaseCancel", resourceCulture);
             }
         }
         
@@ -948,6 +939,15 @@ namespace Resources.JSMessages {
         public static string RequestMediation {
             get {
                 return ResourceManager.GetString("RequestMediation", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Request rate.
+        /// </summary>
+        public static string RequestRate {
+            get {
+                return ResourceManager.GetString("RequestRate", resourceCulture);
             }
         }
         
